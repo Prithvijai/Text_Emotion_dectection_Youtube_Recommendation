5e23245fb0e4ce6c40fc9cb03c67d66f3c6cfe73
prithvijai <prithvijai0510@gmail.com>
2023-01-09 13:01:27 +0530

    Added the lib

---
+++ text.py
@@ -0,0 +1,3 @@
+# this is First time pushing in github
+# This Project will read the text mesg in comment sections and identify the emotion behind it and recommend
+# Youtube video based on that not the previous history 
